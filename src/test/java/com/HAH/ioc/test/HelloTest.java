8bf9cac7fe78e7481659a608838465ae2f4d6413
hhtun137 <htunaunghlaing199@gmail.com>
2024-02-29 10:52:02 +0630

    AnnotationConfigWithMultipleRefresh

--- src/test/java/com/HAH/ioc/test/HelloTest.java
+++ src/test/java/com/HAH/ioc/test/HelloTest.java
@@ -2,23 +2,18 @@ package com.HAH.ioc.test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.HAH.ioc.HelloWorld;
 
 public class HelloTest {
 	
 	@Test
 	public void DemoTest() {
-		var context = new AnnotationConfigApplicationContext();
-		context.register(HelloWorld.class);
-		//context.scan("com.HAH.ioc.HelloWorld");
+		var context = new ClassPathXmlApplicationContext("context.xml");
+		context.refresh();
 		context.refresh();
 		var bean =context.getBean(HelloWorld.class);
 		bean.getWorld();		
 	}
-//	@Test
-//	public void DemoTest() {
-//		var context = new AnnotationConfigApplicationContext(HelloWorld.class);
-//		var bean =context.getBean(HelloWorld.class);
-//		bean.getWorld();		
-//	}
 }

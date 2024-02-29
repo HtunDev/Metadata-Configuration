68ba29a067a9df9b3a8d57523b8feb4ab2f93a12
hhtun137 <htunaunghlaing199@gmail.com>
2024-02-29 10:29:01 +0630

    ClassPathXmlConfig

--- src/test/java/com/HAH/ioc/test/HelloTest.java
+++ src/test/java/com/HAH/ioc/test/HelloTest.java
@@ -1,25 +1,16 @@
 package com.HAH.ioc.test;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.context.support.GenericXmlApplicationContext;
-
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.HAH.ioc.HelloWorld;
 
 public class HelloTest {
 	
-//	@Test
-//	public void DemoTest() {
-//		var context = new GenericXmlApplicationContext();
-//		context.load("context.xml");
-//		context.refresh();
-//		var bean =context.getBean(HelloWorld.class);
-//		bean.getWorld();		
-//	}
 	@Test
 	public void DemoTest() {
-		var context = new GenericXmlApplicationContext("context.xml");
+		var context = new ClassPathXmlApplicationContext("context.xml");
+		context.refresh();
 		var bean =context.getBean(HelloWorld.class);
 		bean.getWorld();		
 	}
-
 }

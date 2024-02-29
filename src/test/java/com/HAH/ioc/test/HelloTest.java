908e34ef30d5fa64c337b17ad8807d309902e838
hhtun137 <htunaunghlaing199@gmail.com>
2024-02-29 10:31:35 +0630

    FileSystemXmlConfig

--- src/test/java/com/HAH/ioc/test/HelloTest.java
+++ src/test/java/com/HAH/ioc/test/HelloTest.java
@@ -1,14 +1,15 @@
 package com.HAH.ioc.test;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
 import com.HAH.ioc.HelloWorld;
 
 public class HelloTest {
 	
 	@Test
 	public void DemoTest() {
-		var context = new ClassPathXmlApplicationContext("context.xml");
+		var context = new FileSystemXmlApplicationContext("context.xml");
 		context.refresh();
 		var bean =context.getBean(HelloWorld.class);
 		bean.getWorld();		
